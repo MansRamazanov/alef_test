144f9ec061ea24b2d5696292d9d7239e2d118f3e
Mans <m.ramazanov@wi-fi.click>
2024-11-11 23:04:14 +0300

    fix validation

--- src/pages/ClientForm.vue
+++ src/pages/ClientForm.vue
@@ -1,44 +1,68 @@
 <template>
   <div class="form-container">
-    <div class="form">
+    <form class="form" @submit.prevent>
       <h2 class="form-title">Персональные данные</h2>
       <div class="input-container">
         <label class="input-label" for="name">Имя</label>
-        <input type="text" v-model="user.name" class="input" id="name" />
+        <input type="text" v-model="user.name" class="input" id="name" maxlength="40" required />
       </div>
       <div class="input-container">
         <label class="input-label" for="age">Возраст</label>
-        <input type="text" v-model.number="user.age" class="input" id="age" />
+        <input
+          type="number"
+          v-model.number="user.age"
+          class="input"
+          id="age"
+          min="0"
+          max="99"
+          step="1"
+          required
+        />
       </div>
       <div class="children-container">
         <div class="children-header">
           <div>Дети (макс.5)</div>
-          <button @click="addChild" :disabled="children.length >= maxChildren" class="add-button">
+          <button
+            type="button"
+            @click="addChild"
+            :disabled="children.length >= maxChildren"
+            class="add-button"
+          >
             <img src="../assets/img/plus.png" alt="Плюс" /> Добавить ребенка
           </button>
         </div>
         <div v-for="(child, index) in children" :key="index" class="children-list">
           <div class="input-container" style="margin-bottom: 0">
             <label class="input-label" for="child_name">Имя</label>
-            <input v-model="child.name" class="input" id="child_name" />
+            <input v-model="child.name" class="input" id="child_name" maxlength="40" required />
           </div>
           <div class="input-container" style="margin-bottom: 0">
             <label class="input-label" for="child_age">Возраст</label>
-            <input v-model.number="child.age" type="text" min="0" class="input" id="child_age" />
+            <input
+              v-model.number="child.age"
+              type="number"
+              class="input"
+              id="child_age"
+              min="0"
+              max="99"
+              step="1"
+              required
+            />
           </div>
 
-          <button @click="removeChild(index)" class="remove-button">Удалить</button>
+          <button type="button" @click="removeChild(index)" class="remove-button">Удалить</button>
         </div>
       </div>
 
       <button
+        type="submit"
         @click="saveData"
         class="save-button"
         :disabled="user.name === '' || user.age === null"
       >
         Сохранить
       </button>
-    </div>
+    </form>
   </div>
 </template>
 
@@ -190,5 +214,11 @@ const saveData = (): void => {
 .add-button:disabled {
   opacity: 0.5;
   cursor: not-allowed;
+}
+
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
 }
 </style>
